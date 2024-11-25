66bfd0de51ec3fb9fa139d35d0b79957cdac4d5a
supra <supra1302@protonmail.com>
2024-11-25 13:56:02 +0000

    .

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,15 +6,7 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		interface Platform {
-			env: {
-				COUNTER: DurableObjectNamespace;
-			};
-			context: {
-				waitUntil(promise: Promise<any>): void;
-			};
-			caches: CacheStorage & { default: Cache }
-		}
+		// interface Platform {}
 	}
 }
 

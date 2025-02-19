969b0d3e793533a322390400ee57dc9f780d736e
supra1302 <davidcatalim@gmail.com>
2025-02-19 18:51:42 +0000

    recruit banner fix

--- src/routes/RecruitBanner.svelte
+++ src/routes/RecruitBanner.svelte
@@ -9,25 +9,28 @@
 	const options: MarqueeckOptions = {
 		speed: 50
 	};
-	console.log(status);
+	// console.log(status);
 </script>
 
 {#await status then recruit}
 	{#if recruit.open}
-		<Marqueeck {options} class="sm:!y-6 !py-4">
-			<!-- Using Tailwind classes -->
-			<div class="flex items-end gap-2 leading-4">
-				<p>
-					<i>{m.recruit_pretitle()}</i>
-				</p>
-				<span class="translate-y-1 text-2xl font-semibold">{m.recruit_title()}</span>
-			</div>
-			<svelte:fragment slot="stickyEnd">
+		<div class="flex items-center">
+			<Marqueeck {options} class="sm:!y-6 !py-4">
+				<!-- Using Tailwind classes -->
+				<div class="flex items-end gap-2 leading-4">
+					<p>
+						<i>{m.recruit_pretitle()}</i>
+					</p>
+					<span class="translate-y-1 text-2xl font-semibold">{m.recruit_title()}</span>
+				</div>
+			</Marqueeck>
+
+			<div class="px-4">
 				<Button href={recruit?.application_url}>
 					{m.recruit_cta()}
 					<ArrowUpRight />
 				</Button>
-			</svelte:fragment>
-		</Marqueeck>
+			</div>
+		</div>
 	{/if}
 {/await}
